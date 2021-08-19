4f1a64c2341bc5b17450ab1f68b25b34156eb70e
bael <osh.kg1819@gmail.com>
2021-08-19 14:23:51 +0600

    add

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,77 +1,66 @@
-import { useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Helper } from '../helper';
 import cls from './Email.module.css'
+import {errors, url} from '../url'
 
 const Email = () => {
-    const url = [
-        {
-            key: 'g',
-            dom: ['g', 'm' , 'a' , 'i' , 'l' , '.' , 'c' , 'o' , 'm']
-        }
-    ]
-
-    const errors = [",", ":", ";", "!", "#", "%", "*", "(", ")", "=", "+", "{", "}", "[", "]/", "'", "/" , ' ']
     const [email , setEmail] = useState('')
     const [warning , setWarning] = useState('')
-    const code = useRef('')
     const [valid , setValid] = useState(false)
     const [showWarning , setShowWarning] = useState(false)
     const symbol = email.indexOf("@");
-    let result
+    let elems
 
+    const email_begin = email?.slice(0 , symbol)
+    const email_key = email?.slice(symbol + 1 , symbol + 2) || 0;
     url.forEach(({key , dom}) => {
-        if(email.toLowerCase().includes(key.toLowerCase())){
-            const res = email.split('')
-            const filter = res.filter(item => item !== key)
-            result = filter.concat(dom)
+        if(email_key === key){
+            elems = email_begin.concat(dom)
+        }else{
+            return
         }
     })
-
-    useRef(code.current = result)
-
-    const submitEmailBtn = e => {
-        e.preventDefault()
-        setShowWarning(true)
-
-        errors.forEach(item => {
-            if(email.toLowerCase().includes(item)){
-                setValid(false)
-                setShowWarning(true)
-                setWarning('Адрес не должен содержать не понятные символы !')
-            }
-        })
-
+    
+    useEffect(() => {
         if(email === ''){
             setValid(false)
             setShowWarning(true)
             setWarning('Введите адрес электронной почты !')
-        }else if(email.indexOf('.') === -1){
-            setShowWarning(true)
-            setValid(false)
-            setWarning('Отсутствуюет символ .')
-        }else if(symbol === -1){
-            setShowWarning(true)
-            setValid(false)
-            setWarning('Отсутствуюет символ @')
-        }else if((symbol < 1) || (symbol > email.length - 5)){
-            setShowWarning(true)
-            setValid(false)
-            setWarning('Адрес электронной почты Неверный')
-        }else{
-            setShowWarning(true)
-            setValid(true)
-            setWarning('Адрес электронной почты был веден ВЕРНО')
+        }else if(email !== ''){
+            if((email.indexOf('.') === -1) || (symbol === -1)){
+                setShowWarning(true)
+                setValid(false)
+                setWarning('Отсутствуют символы . или @')
+            }else if((symbol < 1) || (symbol > email.length - 10)){
+                setShowWarning(true)
+                setValid(false)
+                setWarning('Адрес электронной почты Неверный')
+            }else{
+                setShowWarning(true)
+                setValid(true)
+                setWarning('Адрес электронной почты был веден ВЕРНО')
+            }
+            errors.forEach(item => {
+                if(email.toLowerCase().includes(item.toLowerCase())){
+                    setValid(false)
+                    setShowWarning(true)
+                    setWarning('Адрес не должен содержать не понятные символы !')
+                }
+            })
         }
+    } , [email])
+
+    const refresh = () => {
+        setEmail(elems)
     }
     
     return (
         <section className={cls.email}>
-            <h2 className={cls.email_title}>Введите свой адрес электронной почты</h2>
+            <h2 className={cls.email_title}>Введите свой адрес электронной почты !</h2>
             <form action="address">
                 <input value={email} onChange={e => setEmail(e.target.value)} placeholder='Email' type="email" />
-                <p onClick={() => setEmail(code.current)}>{code.current}</p>
+                <p style={{display: elems ? 'block' : 'none'}} onClick={refresh}>{elems}</p>
                 <Helper valid={valid} appear={showWarning} text={warning}/>
-                <button onClick={submitEmailBtn} type='submit'>ПОДТВЕРДИТЬ</button>
             </form>
         </section>
     )
---
+++ src/url/index.jsx
@@ -0,0 +1,17 @@
+
+export const url = [
+    {
+        key: 'g',
+        dom: '@gmail.com'
+    },
+    {
+        key: 'y',
+        dom: '@yahoo.com'
+    },
+    {
+        key: 'm',
+        dom: '@mail.com'
+    }
+]
+
+export const errors = [",", ":", ";", "!", "#", "%", "*", "(", ")", "=", "+", "{", "}", "[", "]", "'", "/" , ' ']
